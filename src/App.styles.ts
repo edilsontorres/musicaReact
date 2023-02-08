56a1ef1439ea0b60f569c7bc687ad9863093f3fc
Edilson Torres <edilsontor77@gmail.com>
2023-02-08 19:07:41 -0300

    Criando style

---
+++ src/App.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Title = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+height: 100px;
+background-color: none;
+color: #FFFFFF;
+font-size: 40px;
+font-weight: 600;
+margin-bottom: 30px;
+`;
