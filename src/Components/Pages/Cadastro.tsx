d8272d1d006b0e50ddd296e46206c5b10743084e
Edilson Torres <edilsontor77@gmail.com>
2023-02-08 21:36:10 -0300

    Redirecionando página lista

--- src/Components/Pages/Cadastro.tsx
+++ src/Components/Pages/Cadastro.tsx
@@ -1,25 +1,32 @@
 import Form from "../Formulario/Form";
 import { api } from "../Services/api";
+import { useNavigate } from 'react-router-dom'
 
 
 export const Cadastro = ()=>{
-
+    
+    const navigate = useNavigate();
+    
     //Gravando dados no DB
     const cadastrar = async(dados:any) => {
         await api.post('/api/musica', dados)
         .then(response => {
-            console.log(response.data)
+            if(response.status === 201){
+                alert("Musica cadastrada com sucesso!");
+                navigate('/lista');
+            }
         })
         .catch(error =>{
             console.log(error)
-        });  
+        });
        
     }
 
+    
+
     return(
         <>
-            <Form cadastrar={cadastrar}/>
-            
+            <Form cadastrar={cadastrar}/> 
         </>
     )
 
