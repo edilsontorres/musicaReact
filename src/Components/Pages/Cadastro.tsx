cad9e5af4454c68b9846c8904f5de0deb1ce07e2
Edilson Torres <edilsontor77@gmail.com>
2023-02-15 10:59:23 -0300

    alterando caminho das rotas

--- src/Components/Pages/Cadastro.tsx
+++ src/Components/Pages/Cadastro.tsx
@@ -2,9 +2,7 @@ import Form from "../Formulario/Form";
 import { api } from "../Services/api";
 import { useNavigate } from 'react-router-dom'
 
-
 export const Cadastro = ()=>{
-    
     const navigate = useNavigate();
     
     //Gravando dados no DB
@@ -13,16 +11,15 @@ export const Cadastro = ()=>{
         .then(response => {
             if(response.status === 201){
                 alert("Musica cadastrada com sucesso!");
-                navigate('/lista');
+                navigate('/');
             }
         })
         .catch(error =>{
             console.log(error)
         });
        
+       
     }
-
-    
 
     return(
         <>
