431caaa30c61e69be74eb5ea828d0fcb5778eac4
Edilson Torres <edilsontor77@gmail.com>
2023-02-15 10:49:13 -0300

    Criando pagina de edicao

---
+++ src/Components/Pages/Editar.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import Editar from "../Editar/Editar.form";
+import { api } from "../Services/api";
+
+export const Update = () => {
+    const navigate = useNavigate();
+    const { id } = useParams();
+    
+
+    
+    //Atualizando informações no DB
+    const atualizar = async(dados:any) => {
+        
+        if(id !== ''){
+            await api.put(`/api/musica/${id}`, dados)
+            .then(response => {
+                if(response.status === 200){
+                    alert("Musica atualizada com sucesso!");
+                    navigate('/lista');
+                }
+            })
+        }
+        
+    }
+    
+    return(
+        <>
+           <Editar atualizar={atualizar} />
+           
+           
+        </>
+    );
+}
