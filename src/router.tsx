5b725a9f46a3603fc02d1a92cadcf1e42f969665
Edilson Torres <edilsontor77@gmail.com>
2023-02-15 11:01:50 -0300

    Configurando caminho das rotas

--- src/router.tsx
+++ src/router.tsx
@@ -2,14 +2,16 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import { Cadastro } from './Components/Pages/Cadastro';
 import { Lista } from './Components/Pages/Lista';
+import { Update } from './Components/Pages/Editar';
 
 export const Router = () => {
     return(
         
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Cadastro />}></Route>
-                <Route path="/lista" element={<Lista />}></Route>
+                <Route path="/" element={<Lista />}></Route>
+                <Route path="/cadastro" element={<Cadastro />}></Route>
+                <Route path="/editar/:id" element={<Update />}></Route>
             </Routes>
         </BrowserRouter>
 
