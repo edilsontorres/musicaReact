70b7f6ecdf248074535149797dae6fea5b6a1dde
Edilson Torres <edilsontor77@gmail.com>
2023-02-24 18:56:03 -0300

    Editando estilo geral

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -2,12 +2,17 @@ import styled from 'styled-components';
 
 export const Title = styled.div`
 display: flex;
+width: 100%;
 align-items: center;
 justify-content: center;
 height: 100px;
 background-color: none;
-color: #FFFFFF;
-font-size: 40px;
-font-weight: 600;
 margin-bottom: 30px;
+`;
+
+export const AreaTitle = styled.div`
+    color: #2E303A;
+    font-size: 40px;
+    font-weight: 600;
+    
 `;
